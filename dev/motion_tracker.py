66d272c474c1718f75ee7796df8ee0b0b6e8b478
tfranch <torben@franch.eu>
2020-03-13 17:50:20 +0100

    Bug fixes

--- dev/motion_tracker.py
+++ dev/motion_tracker.py
@@ -10,20 +10,19 @@ def main():
     sense.low_light = True
     sense.set_imu_config(True, True, True)
     sensor_start = sense.get_accelerometer()
-    sensor_sensitivity = .01
+    sensor_sensitivity = .0001
     move_list = []
     ex_coll = get_db('zs5_mongo', 'sensor')['freezer']
     while True:
         sensor_in = sense.get_accelerometer()
-        if (sensor_in['roll'] - sensor_start['roll']) ** 2 + (sensor_in['pitch'] - sensor_start['pitch']) ** 2\
-                + (sensor_in['yaw'] - sensor_start['yaw']) ** 2 >= sensor_sensitivity:
-            sense.show_letter('M')
-            move_list.append({'move_ts': datetime.utcnow().timestamp()})
+        move_size = (sensor_in['roll'] - sensor_start['roll']) ** 2 + (
+                    sensor_in['pitch'] - sensor_start['pitch']) ** 2 + (sensor_in['yaw'] - sensor_start['yaw']) ** 2
         sensor_start = sensor_in
-        if len(move_list) > 10:
-            ex_coll.insert_many(move_list)
-            move_list = []
-        time.sleep(.1)
+        if move_size >= sensor_sensitivity:
+            sense.show_letter('M')
+            move_dict = {'move_ts': datetime.utcnow().timestamp(), **sensor_in}
+            ex_coll.insert_one(move_dict)
+        time.sleep(.2)
         sense.clear()
 
 
